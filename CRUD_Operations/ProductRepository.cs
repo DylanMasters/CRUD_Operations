ebdb88af9552b06eec8512bcad91127cb457547a
Dylan Masters <Dy1anmasters93@gmail.com>
2020-03-04 19:50:51 -0600

    Made CreateProduct method that inserts fields into your MySQL server.

--- CRUD_Operations/ProductRepository.cs
+++ CRUD_Operations/ProductRepository.cs
@@ -16,6 +16,7 @@ namespace CRUD_Operations
         
         public List<Product> GetProducts()
         {
+            //This method should be able to select fields from your MySql server and display them. 
             MySqlConnection conn = new MySqlConnection(connString);
             List<Product> products = new List<Product>();
 
@@ -41,6 +42,25 @@ namespace CRUD_Operations
                 }
             }
             return products;
+        }
+        public void CreateProduct(Product prod)
+        {
+            //This method should be able to insert fields into your MySQL server.
+            //Note, if you want to see changes you will need to run GetProducts(); again.
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO products (Name, Price, CategoryID, OnSale) VALUES (@n, @p, @cID, @sale);";
+                cmd.Parameters.AddWithValue("n", prod.Name);
+                cmd.Parameters.AddWithValue("p", prod.Price);
+                cmd.Parameters.AddWithValue("cID", prod.CategoryID);
+                cmd.Parameters.AddWithValue("sale", prod.OnSale);
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
