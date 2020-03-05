687b3bf3f78d80853a1025a3d62e43e394be43e7
Dylan Masters <Dy1anmasters93@aol.com>
2020-03-04 18:47:51 -0600

    Initial commit

---
+++ CRUD_Operations/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_Operations
+{
+    class ProductRepository
+    {
+        public string connString;
+
+        public ProductRepository(string _connectionString)
+        {
+            connString = _connectionString;
+        }
+        
+    }
+}
---
+++ CRUD_Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using MySql.Data.MySqlClient;
+
+namespace CRUD_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            string jsonText = File.ReadAllText("appsettings.development.json");
+#else
+            string jsonText = File.ReadAllText("appsettings.release.json");
+#endif
+            var connString = JObject.Parse(jsonText)["ConnectionStrings"]["DefaultConnection"].ToString();
+          
+        }
+    }
+}
