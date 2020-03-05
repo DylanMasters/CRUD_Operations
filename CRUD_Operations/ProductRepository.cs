274510663cde7421cb1af381a77ca21af1f018d1
Dylan Masters <Dy1anmasters93@gmail.com>
2020-03-04 19:34:35 -0600

    made GetProducts method that reads the information from a  dapper SQL query

--- CRUD_Operations/ProductRepository.cs
+++ CRUD_Operations/ProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace CRUD_Operations
 {
@@ -13,5 +14,33 @@ namespace CRUD_Operations
             connString = _connectionString;
         }
         
+        public List<Product> GetProducts()
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+            List<Product> products = new List<Product>();
+
+            using (conn)
+            {
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT Stocklevel, Name, Price, ProductID FROM products;";
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        StockLevel = (string)reader["StockLevel"],
+                        Name = (string)reader["Name"],
+                        Price = (decimal)reader["Price"],
+                        ProductID = (int)reader["ProductID"]
+                    };
+                    products.Add(product);
+                    Console.WriteLine($"{product.ProductID}...{product.Name}...{product.Price}...{product.StockLevel}");
+                }
+            }
+            return products;
+        }
     }
 }
